92cdba4c8cf5377081eefc14a80501c8bfe84e39
spv-dev <psjob@bk.ru>
2025-09-28 18:13:49 +0300

    123332

--- v2/main.go
+++ v2/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(getVersion())
+}
+
+func getVersion() string {
+	cmd := exec.Command("git", "describe", "--tags", "--always", "--dirty", "--match", "v*")
+	output, err := cmd.Output()
+	if err != nil {
+		return "v0.0.0-unknown"
+	}
+	return strings.TrimSpace(string(output))
 }
